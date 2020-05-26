17491272a65d46fe857305ddeda12fa91506919e
Pouriataghipour <59606980+Pouria83@users.noreply.github.com>
2020-05-26 12:47:13 +0430

    Add files via upload

---
+++ Quadratic equation.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+#Coder Name: Pouria-Taghipour
+#website:pcvitamin.ir
+#version:1.0
+#strat
+import math
+a=int(input("Enter Your  Number A -->  :"))
+b=int(input("Enter Your  Number B -->  :"))
+c=int(input("Enter Your  Number C -->  :"))
+#********************************************
+print("********************************************")
+print("             ∆=0 Moadele Yek javab darad        ")
+print("             ∆>0 Moadele 2  javab darad            ")
+print("             ∆<0 Moadele Yek javab darad         ")
+print("                     Formuls∆=(b^2-4ac)                   ")
+print("                 Formuls X=(-b±√∆)/2a                   ")
+print("********************************************")
+delta=b*b-4*a*c
+print("Delta =",delta)
+
+#********************************************
+
+if delta==0:
+    x=(-b+math.sqrt(delta))/(2*a)
+    print("Moadele Yek javab darad.")
+    print("X=",x)
+
+#*******************************************   
+
+elif delta<0:
+        print("Moadele javab nadarad!")
+
+#*******************************************        
+else:
+
+        print("Moadele 2  javab darad")
+        x1=(-b+math.sqrt(delta))/(2*a)
+        x2=(-b+-math.sqrt(delta))/(2*a) 
+        print("X1=",x1)
+        print("X2=",x2)
+
+#*******************************************       
+
+#End
+        
+
+    
+    
