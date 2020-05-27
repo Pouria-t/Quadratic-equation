ad79e4b15492220becf30939b5fe85b3af4665ac
Pouria-Taghipour <59606980+Pouria83@users.noreply.github.com>
2020-05-27 16:09:26 +0430

    Update Quadratic equation.py

--- Quadratic equation.py
+++ Quadratic equation.py
@@ -3,7 +3,7 @@
 #website:pcvitamin.ir
 #version:1.0
 #strat
-import math
+form math import*
 a=int(input("Enter Your  Number A -->  :"))
 b=int(input("Enter Your  Number B -->  :"))
 c=int(input("Enter Your  Number C -->  :"))
@@ -21,7 +21,7 @@ print("Delta =",delta)
 #********************************************
 
 if delta==0:
-    x=(-b+math.sqrt(delta))/(2*a)
+    x=(-b+sqrt(delta))/(2*a)
     print("Moadele Yek javab darad.")
     print("X=",x)
 
@@ -34,7 +34,7 @@ elif delta<0:
 else:
 
         print("Moadele 2  javab darad")
-        x1=(-b+math.sqrt(delta))/(2*a)
+        x1=(-b+sqrt(delta))/(2*a)
         x2=(-b+-math.sqrt(delta))/(2*a) 
         print("X1=",x1)
         print("X2=",x2)
